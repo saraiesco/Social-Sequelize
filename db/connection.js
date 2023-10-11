ca25847ec9883a8c503727f3d231b888b447cd9a
saraiesco <saraiescobar@google.com>
2023-10-11 09:00:31 -0500

    connection

--- db/connection.js
+++ db/connection.js
@@ -1,9 +1,14 @@
+const path = requre("path");
+const {Sequelize, Datatypes , Model} = require("sequelize");
 
-
-
+const db = new Sequelize({
+    dialect: "sqlite",
+    storage: path.join(__dirname, "db.sqlite"),
+});
 
 
 module.exports = {
     db,
-    
-}
+    Datatypes,
+    Model,
+};
